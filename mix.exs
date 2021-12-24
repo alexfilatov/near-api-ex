95316880dbed3cd68cc3449ca7934da078531be8
Alex Filatov <alex@alexfilatov.com>
2021-12-24 20:10:41 +0000

    attempt to run CI on 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NearApi.MixProject do
     [
       app: :near_api,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
