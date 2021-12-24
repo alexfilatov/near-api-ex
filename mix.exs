d153ca470c82473cdd54a79b51a9b0802890c5ca
Alex Filatov <alex@alexfilatov.com>
2021-12-24 20:12:04 +0000

    bumped min elixir version yo 1.9 because of Config

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NearApi.MixProject do
     [
       app: :near_api,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
