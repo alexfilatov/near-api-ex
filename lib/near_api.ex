7b2fb6cf1782d564ba53426d292fce40aebf62c1
Alex Filatov <alex@alexfilatov.com>
2021-12-26 17:20:30 +0000

    delegated `.view_access_key_list`

--- lib/near_api.ex
+++ lib/near_api.ex
@@ -4,4 +4,6 @@ defmodule NearApi do
   """
 
   defdelegate view_access_key(account_id), to: NearApi.AccessKeys
+
+  defdelegate view_access_key_list(account_id), to: NearApi.AccessKeys
 end
--- test/near_api_test.exs
+++ test/near_api_test.exs
@@ -8,6 +8,7 @@ defmodule NearApiTest do
   describe "NearApi" do
     test "AccessKeys: module contains delegated functions", %{functions: functions} do
       refute functions -- [view_access_key: 1] == functions
+      refute functions -- [view_access_key_list: 1] == functions
     end
   end
 end
