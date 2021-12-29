0f376d6ad2bb0bbad7845a98b0a34f5ea6e74909
Alex Filatov <alex@alexfilatov.com>
2021-12-29 01:55:48 +0000

    [Contracts API] refactored payload in Accounts API

--- lib/near_api/accounts.ex
+++ lib/near_api/accounts.ex
@@ -3,6 +3,7 @@ defmodule NearApi.Accounts do
   NEAR RPC - Accounts API
   """
   import NearApi.Utils, only: [api_call: 1, api_call_experimental: 2]
+  import NearApi.Payload
 
   @doc """
   Returns basic account information
@@ -11,16 +12,8 @@ defmodule NearApi.Accounts do
           {:ok, body :: map}
           | {:error, error_message: error_message :: String.t(), response: response :: map}
   def view_account(account_id, block_id \\ nil) do
-    payload = payload_view_account("view_account", account_id, block_id)
+    payload = payload("view_account", account_id, block_id)
     api_call(payload)
-  end
-
-  defp payload_view_account(request_type, account_id, nil) do
-    %{request_type: request_type, finality: "final", account_id: account_id}
-  end
-
-  defp payload_view_account(request_type, account_id, block_id) do
-    %{request_type: request_type, block_id: block_id, account_id: account_id}
   end
 
   @doc """
@@ -31,15 +24,7 @@ defmodule NearApi.Accounts do
           {:ok, body :: map}
           | {:error, error_message: error_message :: String.t(), response: response :: map}
   def account_changes(account_ids, block_id \\ nil) do
-    payload = payload_account_changes("account_changes", account_ids, block_id)
+    payload = payload_experimental("account_changes", account_ids, block_id)
     api_call_experimental(payload, "EXPERIMENTAL_changes")
-  end
-
-  defp payload_account_changes(changes_type, account_ids, nil) do
-    %{changes_type: changes_type, account_ids: account_ids, finality: "final"}
-  end
-
-  defp payload_account_changes(changes_type, account_ids, block_id) do
-    %{changes_type: changes_type, account_ids: account_ids, block_id: block_id}
   end
 end
