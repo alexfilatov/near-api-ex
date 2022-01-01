de53477d33be8e3f5a7c5094e305ee4e6713f99f
Alex Filatov <alex@alexfilatov.com>
2022-01-01 22:00:53 +0000

    get block by block_id

--- test/near_api/block_test.exs
+++ test/near_api/block_test.exs
@@ -23,59 +23,59 @@ defmodule NearApi.BlockTest do
     end
 
     test "success: block with block_id" do
-#      use_cassette "block/success_block_id" do
-        block_id = "8H9Bop1P4LtZMr4Xkne5CQHcnmiJFWzF77F4v3kMtYwT"
-        {:ok, body} = API.block("client.chainlink.testnet", block_id)
-        assert body["result"]["block_hash"] == block_id
+      use_cassette "block/success_block_id" do
+        block_id = "DnhS8YtJQkJ9jT2nCmLNa7R38neWKmUfK6BqgBmBo3Y7"
+        {:ok, body} = API.block(block_id)
+        assert body["result"]["header"]["hash"] == block_id
         refute body["result"]["error"]
-#      end
+      end
     end
 
     test "error: block with with wrong block_id" do
-#      use_cassette "block/error" do
-        block_id = "8H9Bop1P4LtZMr4Xkne5CQHcnmiJFWzF77F4v3kMtYw1"
-        {:error, error} = API.block("client.chainlink.testnet", block_id)
-        assert error.error_cause == "UNKNOWN_BLOCK"
-        assert String.match?(error.error_description, ~r/DB Not Found Error/)
-#      end
+      #      use_cassette "block/error" do
+      block_id = "8H9Bop1P4LtZMr4Xkne5CQHcnmiJFWzF77F4v3kMtYw1"
+      {:error, error} = API.block("client.chainlink.testnet", block_id)
+      assert error.error_cause == "UNKNOWN_BLOCK"
+      assert String.match?(error.error_description, ~r/DB Not Found Error/)
+      #      end
     end
   end
 
   describe ".changes_in_block_experimental" do
     test "success: changes_in_block_experimental with finality: final" do
-#      use_cassette "changes_in_block_experimental/success" do
-        {:ok, body} = API.block("yellowpie.testnet")
-        assert body["id"] == "dontcare"
-        assert body["jsonrpc"] == "2.0"
-        assert body["result"]["block_height"]
-        assert body["result"]["block_hash"]
-        assert is_list(body["result"]["proof"])
-        assert is_list(body["result"]["values"])
-        refute body["result"]["error"]
-#      end
+      #      use_cassette "changes_in_block_experimental/success" do
+      {:ok, body} = API.block("yellowpie.testnet")
+      assert body["id"] == "dontcare"
+      assert body["jsonrpc"] == "2.0"
+      assert body["result"]["block_height"]
+      assert body["result"]["block_hash"]
+      assert is_list(body["result"]["proof"])
+      assert is_list(body["result"]["values"])
+      refute body["result"]["error"]
+      #      end
     end
 
     test "success: changes_in_block_experimental with block_id" do
-#      use_cassette "changes_in_block_experimental/success_block_id" do
-        block_id = "F4xEAQHe1fUwDzyha759UTr1WF8ozVukZdioVAb9yWGa"
-        {:ok, body} = API.block("yellowpie.testnet", block_id)
-        assert body["result"]["block_hash"] == block_id
-        refute body["result"]["error"]
-#      end
+      #      use_cassette "changes_in_block_experimental/success_block_id" do
+      block_id = "F4xEAQHe1fUwDzyha759UTr1WF8ozVukZdioVAb9yWGa"
+      {:ok, body} = API.block("yellowpie.testnet", block_id)
+      assert body["result"]["block_hash"] == block_id
+      refute body["result"]["error"]
+      #      end
     end
 
     test "error: changes_in_block_experimental state is too large" do
-#      use_cassette "changes_in_block_experimental/error_too_large" do
-        {:error, error} = API.block("client.chainlink.testnet")
-        assert error.error_cause == "TOO_LARGE_CONTRACT_STATE"
-        assert error.error_code == -32000
+      #      use_cassette "changes_in_block_experimental/error_too_large" do
+      {:error, error} = API.block("client.chainlink.testnet")
+      assert error.error_cause == "TOO_LARGE_CONTRACT_STATE"
+      assert error.error_code == -32000
 
-        assert error.error_description ==
-                 "State of contract client.chainlink.testnet is too large to be viewed"
+      assert error.error_description ==
+               "State of contract client.chainlink.testnet is too large to be viewed"
 
-        assert error.error_message == "Server error"
-        assert error.error_type == "HANDLER_ERROR"
-#      end
+      assert error.error_message == "Server error"
+      assert error.error_type == "HANDLER_ERROR"
+      #      end
     end
   end
 end
