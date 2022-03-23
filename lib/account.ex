791cb0b04c0973fcfbdcb905c044c4f5732d9a83
Alex Filatov <alex@alexfilatov.com>
2022-03-23 21:43:14 +0000

    implemented Account.send_money for FUllAccess keys

--- lib/account.ex
+++ lib/account.ex
@@ -1,20 +1,38 @@
 defmodule NearApi.Account do
   @moduledoc """
-  NearApi [accounts]().
+  NearApi Account struct
+  Contains account_id, e.g. `hellowworld.testnet`, and KeyPair (with public and secret keys)
   """
 
-  @typedoc """
-
-  """
   @type t :: %__MODULE__{
-          signer?: boolean(),
-          writable?: boolean(),
-          key: NearApi.key() | nil
+          account_id: String.t(),
+          key_pair: NearApi.KeyPair.t() | nil
         }
 
   defstruct [
-    :key,
-    signer?: false,
-    writable?: false
+    :account_id,
+    :key_pair
   ]
+
+  @doc """
+  Send money from one Near account to another.
+  Technically this is a wrapper around `Transfer` transaction
+    `from_account` - an account strut NearApi.Account where `account_id` and `key_pair`
+      - `key_pair` must be FullAccess key and Transfers only available with this permissions level
+    `receiver_id` - string value of the Near account ID, e.g. helloworld.testnet
+    `amount` - as String because in Yocto
+  """
+  @spec send_money(from_account :: NearApi.Account.t(), receiver_id :: String.t(), amount :: String.t()) :: nil
+  def send_money(from_account, receiver_id, amount) do
+    actions = [%NearApi.Actions.Transfer{deposit: amount}]
+    {:ok, tx} = NearApi.Transaction.create_transaction(from_account, receiver_id, actions)
+
+    tx
+    |> NearApi.Transaction.payload(from_account.key_pair)
+    |> NearApi.RPC.Transactions.send_transaction_commit()
+  end
+
+  def build_account(account_id, key_pair) do
+    %__MODULE__{account_id: account_id, key_pair: key_pair}
+  end
 end
--- lib/transaction.ex
+++ lib/transaction.ex
@@ -21,8 +21,7 @@ defmodule NearApi.Transaction do
       receiver_id: :string,
       block_hash: [32],
       actions: [:borsh]
-    ],
-    enum_map: NearApi.Action.enum_map()
+    ]
 
   defstruct [
     :signer_id,
@@ -32,6 +31,43 @@ defmodule NearApi.Transaction do
     :block_hash,
     :actions
   ]
+
+  @doc """
+  Creates a NearApi.Transaction struct, not serialized
+  Parameters:
+    `from_account` - an account strut NearApi.Account where `account_id` and `key_pair`
+    `receiver_id` - NEAR account ID who we are sending tokens, e.g. `helloworld.near`
+    `actions` - a list of transaction actions, e.g. NearApi.Actions.FunctionCall or NearApi.Actions.Transfer
+  """
+  @spec create_transaction(from_account :: NearApi.Account.t(), receiver_id :: String.t(), actions :: list) ::
+          {:ok, NearApi.Transaction.t()} | {:error, :error_retrieving_access_key}
+  def create_transaction(from_account, receiver_id, actions) do
+    public_key = from_account.key_pair.public_key
+    public_key_encoded = B58.encode58(from_account.key_pair.public_key.data)
+    account_id = from_account.account_id
+
+    with {:ok, key} <- NearApi.RPC.AccessKeys.view_access_key(account_id, nil, public_key_encoded) do
+      block_hash_raw = key["result"]["block_hash"]
+      nonce = key["result"]["nonce"] + 1
+
+      block_hash = B58.decode58!(block_hash_raw)
+
+      {:ok,
+       %NearApi.Transaction{
+         signer_id: account_id,
+         receiver_id: receiver_id,
+         nonce: nonce,
+         public_key: public_key,
+         block_hash: block_hash,
+         actions: actions
+       }}
+    else
+      error ->
+        Logger.error("#{__MODULE__}: Cannot retrieve access key: #{inspect(error)}")
+        {:error, :error_retrieving_access_key}
+    end
+  end
+
 
   def sign_and_serialise(tx, key_pair) do
     serialised_tx = borsh_encode(tx)
@@ -43,6 +79,8 @@ defmodule NearApi.Transaction do
   end
 
   def payload(tx, key_pair) do
-    sign_and_serialise(tx, key_pair) |> Base.encode64(padding: false)
+    tx
+    |> sign_and_serialise(key_pair)
+    |> Base.encode64(padding: false)
   end
 end
